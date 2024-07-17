5ddad768fd6f9d72204489e41be4974a65a64ac1
sykul <luke.flatley@outlook.com>
2024-07-17 18:44:27 +0200

    uudpate

--- script.js
+++ script.js
@@ -22,36 +22,43 @@ const postcodeRegexes = [
 
 const countryCodes = postcodeRegexes.map((object) => object.country);
 
-function validateEmail(address) {
-  if (address.value && address.checkValidity()) {
-    return true;
+function validateEmail(emailField) {
+
+  if (emailField.value.length === 0) {
+    emailField.setCustomValidity('Please type an email address')
   }
 
-  const parts = address.value.split("@");
+
+  const parts = emailField.value.split("@");
 
   if (parts.length !== 2) {
+    emailField.setCustomValidity("invalid email address");
     return false;
   }
 
   if (parts[0].trim().length === 0 || parts[1].trim().length === 0) {
     return false;
   }
+  if (emailField.value) {
+    emailField.setCustomValidity('');
+    return true;
+  }
 
-  return true;
+  emailField.setCustomValidity('');
+  return false;
 }
 
-function validateCountry() {
+function validateCountry(countryField) {
   if (countryField.value === "") {
     return false;
   } else if (countryCodes.includes(countryField.value)) {
-    console.log(countryField.value);
     return true;
   } else {
-    console.log("country?");
+    return false;
   }
 }
 
-function validatePostcode() {
+function validatePostcode(postcodeField, countryField, postcodeRegexes) {
   const postcodeRegexObject = postcodeRegexes.filter(
     (item) => item.country === `${countryField.value}`
   );
@@ -61,22 +68,56 @@ function validatePostcode() {
 
 function validatePassword(passwordField) {
   if (passwordField.value.length < 5) {
-    return 'needs to be between 5 and 20 characters'
+    return "needs to be between 5 and 20 characters";
   } else if (passwordField.value.length > 20) {
-    return 'needs to be between 5 and 20 characters'
+    return "needs to be between 5 and 20 characters";
   } else if (/\%/.test(passwordField.value)) {
-    return "can't contain '%'"
+    return "can't contain '%'";
   } else {
     return true;
   }
 }
 
+function checkPasswordsMatch(passwordField, passwordConfirmationField) {
+  if (passwordField.value === passwordConfirmationField.value) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+emailField.addEventListener("change", () => validateEmail(emailField));
+
+countryField.addEventListener("change", () => validateCountry(countryField));
+
+postcodeField.addEventListener("change", () =>
+  validatePostcode(postcodeField, countryField, postcodeRegexes)
+);
+
+passwordField.addEventListener("change", () => validatePassword(passwordField));
+
+passwordConfirmationField.addEventListener("change", () =>
+  checkPasswordsMatch(passwordField, passwordConfirmationField)
+);
+
 submitButton.addEventListener("click", () => {
   console.log(`email: ${validateEmail(emailField)}`);
-  console.log(`country: ${validateCountry(emailField)}`);
-  console.log(`postcode: ${validatePostcode(countryField)}`);
+  console.log(`country: ${validateCountry(countryField)}`);
+  console.log(
+    `postcode: ${validatePostcode(
+      postcodeField,
+      countryField,
+      postcodeRegexes
+    )}`
+  );
   console.log(`password: ${validatePassword(passwordField)}`);
-})
+  console.log(
+    `password match: ${checkPasswordsMatch(
+      passwordField,
+      passwordConfirmationField
+    )}`
+  );
+});
 
 countryField.value = "GB";
 submitButton.click();
